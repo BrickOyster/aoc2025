67f7a6a38c9aba8566c528871d5e449a2d6cc40b
brickoyster <dimitris2002keramidas@gmail.com>
2025-12-05 15:14:53 +0200

    day 2 done

---
+++ 2/2.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# aocli view -d 2 -y 2025
+time {
+part1=0; part2=0
+
+while IFS= read -r i; do
+    start=${i%-*}
+    end=${i#*-}
+    for num in $(seq $start $end); do
+        if [[ $(echo $num | grep -E '^([0-9]+)\1+$') ]]; then
+            (( part2 += $num ))
+            if [[ $(echo $num | grep -E '^([0-9]+)\1$') ]]; then
+                (( part1 += $num ))
+            fi
+        fi
+    done
+done < <(tr ',' '\n' < 2.in)
+
+echo "Part 1: $part1"
+# aocli submit -d 2 -y 2025 -p 1 26255179562
+echo "Part 2: $part2"
+# aocli submit -d 2 -y 2025 -p 2
+}
+echo "Done!"
